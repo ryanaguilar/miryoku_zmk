530c5b6509f398da9b9c86ce9353476acf9520ec
Ryan Aguilar <ryanaguilar323@gmail.com>
2025-12-11 14:02:56 -0500

    Incorporate timeless

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -11,12 +11,15 @@
 // Helper macro to use the hold-preferred behavior for U_LT_SYM at position 35
 #define U_LT_SYM_HP(LAYER, TAP) &u_lt_sym_hp35 LAYER TAP
 
+#define HML(LAYER, TAP) &hml LAYER TAP
+#define HMR(LAYER, TAP) &hmr LAYER TAP
+
 // Override QWERTY base layer to use hold-preferred for:
 // - Left pinkie bottom row (Z key at position 20) uses U_LT_HP instead of U_LT
 // - Right thumb SYM key (RET at position 35) uses U_LT_SYM_HP instead of U_LT_SYM
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTY \
 &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
+HML(LGUI, A),     HML(LALT, S),     HML(LCTRL, D),    HMR(LSHFT, F),    &kp G,             &kp H,             HMR(LSHFT, J),    HMR(LCTRL, K),    HMR(LALT, L),     HMR(LGUI, SQT),   \
 U_LT_HP(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT_SYM_HP(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
